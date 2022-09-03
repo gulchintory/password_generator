becf3304d184dce6ec507f3fada22758202f08f9
gulchintory <avgulcin@gmail.com>
2022-09-03 20:34:51 +1000

    password generator complete

--- script.js
+++ script.js
@@ -13,5 +13,66 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  
+  var passlength = window.prompt("Enter the length of the password between 8 - 128");
+   // If user pressed Cancel, immediately end function
+   if (!passlength || passlength <8 || passlength > 128) {
+    window.alert("Password lenght is not correct");
+    return;
+  }
+
+  var isLowercase = window.prompt("If you want your password include lowercase, then type yes");
+
+  var isUppercase = window.prompt("If you want your password include uppercase, then type yes");
+
+  var isNumeric = window.prompt("If you want your password include numeric, then type yes");
+
+  var isSpecial = window.prompt("If you want your password include special characters, then type yes");
+
+  var lowerCases = 'abcdefghijklmnopqrstuvwxyz'
+
+  var upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  var numerics = '0123456789'
+
+  var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  var password = ''
+
+  if (isLowercase === 'yes') {
+    // Get random index from array of options
+  var index = Math.floor(Math.random() * lowerCases.length);
+    password += lowerCases[index];
+  }
+
+  if (isUppercase === 'yes') {
+    // Get random index from array of options
+  var index = Math.floor(Math.random() * upperCases.length);
+    password += upperCases[index];
+  }
+
+  if (isNumeric === 'yes') {
+    // Get random index from array of options
+  var index = Math.floor(Math.random() * numerics.length);
+    password += numerics[index];
+  }
+
+  if (isSpecial === 'yes') {
+    // Get random index from array of options
+  var index = Math.floor(Math.random() * specialCharacters.length);
+    password += specialCharacters[index];
+  }
+
+  while (password.length < passlength) {
+    // Get random index from array of options
+    var index = Math.floor(Math.random() * specialCharacters.length);
+    password += specialCharacters[index];
+  }
+  
+
+  return password
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
